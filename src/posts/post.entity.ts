af0e2dc49254eb831be21ef5872343a1730a72c6
Ye Thiha <mightycryptowall@gmail.com>
2021-10-22 23:32:58 +0630

    category entity -a

--- src/posts/post.entity.ts
+++ src/posts/post.entity.ts
@@ -1,5 +1,6 @@
+import Category from "src/categories/category.entity";
 import User from "src/users/user.entity";
-import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, JoinTable, ManyToMany, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 
 
 @Entity()
@@ -23,6 +24,10 @@ class Post{
     
     @ManyToOne(() => User, (author: User) => author.posts)
     public author: User;
+
+    @ManyToMany(() => Category,(category: Category) => category.posts)
+    @JoinTable()
+    public categories: Category[];
 
 }
 
