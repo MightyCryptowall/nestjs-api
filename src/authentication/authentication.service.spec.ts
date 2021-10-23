ac5a6e1ea2b3bb5d25a569f7bc2fc932e3ca21cc
Ye Thiha <mightycryptowall@gmail.com>
2021-10-23 18:15:50 +0630

    test module -a authentication.spec

--- src/authentication/authentication.service.spec.ts
+++ src/authentication/authentication.service.spec.ts
@@ -1,36 +1,58 @@
-import { ConfigService } from '@nestjs/config';
-import { JwtService } from '@nestjs/jwt';
+import { DatabaseModule } from './../database/database.module';
+import { UsersModule } from './../users/users.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule, JwtService } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
 import User from '../users/user.entity';
 import { UsersService } from '../users/users.service';
 
 import { Repository } from 'typeorm';
 import { AuthenticationService } from './authentication.service';
+import * as Joi from '@hapi/joi';
 
 describe('AuthenticationService', () => {
   let authenticationService: AuthenticationService;
 
-  beforeEach(() => {
-    authenticationService = new AuthenticationService(
-      new UsersService(
-        new Repository<User>()
-      ),
-      new JwtService({
-        secretOrPrivateKey: "Secret key"
-      }),
-      new ConfigService()
-    );
-  })
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      imports: [
+        UsersModule,
+        ConfigModule.forRoot({
+          validationSchema: Joi.object({
+            POSTGRES_HOST: Joi.string().required(),
+            POSTGRES_PORT: Joi.number().required(),
+            POSTGRES_USER: Joi.string().required(),
+            POSTGRES_PASSWORD: Joi.string().required(),
+            POSTGRES_DB: Joi.string().required(),
+            JWT_SECRET: Joi.string().required(),
+            JWT_EXPIRATION_TIME: Joi.string().required(),
+            PORT: Joi.number(),
+          })
+        }),
+        DatabaseModule,
+        JwtModule.registerAsync({
+          imports: [ConfigModule],
+          inject: [ConfigService],
+          useFactory: async (configService: ConfigService) => ({
+            secret: configService.get('JWT_SECRET'),
+            signOptions: {
+              expiresIn: `${configService.get('JWT_EXPIRATION_TIME')}`,
+            },
+          }),
+        }),
+      ],
+      providers: [AuthenticationService],
+    }).compile();
 
-  
-  describe("when creating a cookie", () => {
-    it("should return a string", () => {
-      const userId = "23ca0e0b-cce0-43e6-8371-059f1f94ea76";
+    authenticationService = await module.get<AuthenticationService>(AuthenticationService);
+  });
+
+  describe('when creating a cookie', () => {
+    it('should return a string', () => {
+      const userId = '23ca0e0b-cce0-43e6-8371-059f1f94ea76';
       expect(
-        typeof authenticationService.getCookieWithJwtToken(userId)
-      ).toEqual("string");
-    })
-  })
-
+        typeof authenticationService.getCookieWithJwtToken(userId),
+      ).toEqual('string');
+    });
+  });
 });
-
